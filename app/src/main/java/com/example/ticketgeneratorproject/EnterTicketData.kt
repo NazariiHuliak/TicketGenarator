308c1c8e70dac4130926ab03c2498eaeb735c3f3
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2023-08-28 17:59:43 +0300

    created main menu of application and EnterData activity

---
+++ app/src/main/java/com/example/ticketgeneratorproject/EnterTicketData.kt
@@ -0,0 +1,11 @@
+package com.example.ticketgeneratorproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterTicketData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_ticket_data)
+    }
+}
--- app/src/main/java/com/example/ticketgeneratorproject/MainActivity.kt
+++ app/src/main/java/com/example/ticketgeneratorproject/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ticketgeneratorproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addBtn = findViewById<Button>(R.id.create_new_ticket)
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, EnterTicketData::class.java)
+            startActivity(intent)
+        }
     }
 }
