98d11591caa9c1a87b02bb520650962daa3ab3a8
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2023-09-22 00:07:52 +0300

    changed validation in address parser

--- app/src/main/java/com/example/ticketgeneratorproject/Entities/Address.kt
+++ app/src/main/java/com/example/ticketgeneratorproject/Entities/Address.kt
@@ -1,5 +1,7 @@
 package com.example.ticketgeneratorproject.Entities
 
+import android.util.Log
+
 data class Address(
     val country: String,
     val city: String,
@@ -8,12 +10,17 @@ data class Address(
 ): java.io.Serializable{
     companion object{
         fun parseAddress(address: String): Address{
-            val components = address.split(", ")
-            if (components.size == 4) {
-                return Address(components[0], components[1], components[2], components[3])
+            var formattedAddress = address.replace(",", "").
+                                    replace(".", "").
+                                    replace("вул", "")
+            while("  " in formattedAddress){
+                formattedAddress = formattedAddress.replace("  ", " ")
             }
-            if (components.size == 3) {
-                return Address("Україна", components[0], components[1], components[2])
+            val components = formattedAddress.split(" ")
+            if (components.size == 4){
+                return Address(components[0], components[1], components[2], components[3])
+            } else if(components.size == 5){
+                return Address(components[0], components[1], components[2] + " " + components[3], components[4])
             }
             return Address("", "", "", "")
         }
