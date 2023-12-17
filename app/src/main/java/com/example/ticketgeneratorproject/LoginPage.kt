bac12a29e40c3494314d8498a22c8d2ba0124494
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2023-12-17 18:39:20 +0200

    created UI

---
+++ app/src/main/java/com/example/ticketgeneratorproject/LoginPage.kt
@@ -0,0 +1,11 @@
+package com.example.ticketgeneratorproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page_layout)
+    }
+}
